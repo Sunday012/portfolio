32c1e8e5cc7ec22ce8d5aa1fe13669b074a7fcf6
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2023-12-22 22:35:03 +0100

    recent commit

---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+import {Loader} from "@/components/intro-loader"
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <h1>Loading...</h1>
+  }
---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const SideItems = [
+    {
+        text: "Linkedin",
+        link:"",
+        src: ""
+    },
+    {
+        text: "GitHub",
+        link:"",
+        src: ""
+    },
+    {
+        text: "Whatsapp",
+        link:"",
+        src: ""
+    }
+]
