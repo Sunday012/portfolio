0f8af525fc2cb739ec896eb151627dcbc04ceb84
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-04-28 00:14:08 +0100

    little animation

--- app/(main)/_components/hero.tsx
+++ app/(main)/_components/hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 import { Header } from "./header";
-import { easeIn, easeInOut, easeOut, motion } from "framer-motion";
+import { animate, easeIn, easeInOut, easeOut, motion } from "framer-motion";
 
 import { SideItems } from "@/constants";
 import Image from "next/image";
@@ -10,11 +10,31 @@ import { Icon } from "@/components/icon";
 
 export const Hero = () => {
  
+  const heroVar = {
+    initial: {
+      y: 100,
+      opacity: 0
+    },
+    animate:{
+      y: 0,
+      opacity: 1,
+      transition:{
+        duration: 2,
+        ease: "easeInOut",
+        delay: 5
+    }
+    }
+  }
+
   return (
     <section className="h-[100vh] w-full dark:bg-[#0a0a0a]">
       <div className="absolute top-0 left-0 w-full h-[100vh]">
         <Header />
-        <div className="flex lg:ml-16 ml-0 lg:mt-20 mt-[74px] lg:px-24 px-10 ">
+        <motion.div className="flex lg:ml-16 ml-0 lg:mt-20 mt-[74px] lg:px-24 px-10 "
+        variants={heroVar}
+        initial="initial"
+        animate="animate"
+        >
           <div className="flex flex-col gap-3">
           <span className="dark:text-[#47eaff] text-blue-800 font-semibold text-xl">
             Hello i&apos;m
@@ -47,7 +67,7 @@ export const Hero = () => {
              className="hidden dark:flex"
             />
           </div>
-        </div>
+        </motion.div>
         <div className="sm:flex fixed gap-3 mt-[90vh] top-0 left-0 ml-[64px] hidden">
           {SideItems.map((items, index) => (
             <Link href={items.link} key={index} className="dark:text-[#47eaff] text-blue-800">
